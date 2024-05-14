a6e75c8ee31283841d439be134a6084b92594b68
JaewooCho <djdlzl93@gmail.com>
2024-05-14 12:04:43 +0900

    hello world

---
+++ src/main/java/btc/study/jaewoocho/JaewoochoApplication.java
@@ -0,0 +1,13 @@
+package btc.study.jaewoocho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaewoochoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaewoochoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/btc/study/jaewoocho/JaewoochoApplicationTests.java
@@ -0,0 +1,13 @@
+package btc.study.jaewoocho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaewoochoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
