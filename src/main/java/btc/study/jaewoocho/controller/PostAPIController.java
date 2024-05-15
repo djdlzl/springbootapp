b0a891161c2487dea5bb7fcb66eed78502311879
JaewooCho <djdlzl93@gmail.com>
2024-05-15 21:49:14 +0900

    get, post api

--- src/main/java/btc/study/jaewoocho/controller/PostAPIController.java
+++ src/main/java/btc/study/jaewoocho/controller/PostAPIController.java
@@ -8,8 +8,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostAPIController {
 
     @PostMapping(value = "/greet")
-    public String greetFunc(@RequestBody String name){
-        return "Hello, " + name;
+    public String greetFunc(@RequestBody(required = false) GreetRequest request){
+        
+        if(request != null){
+            return "Hello, " + request.getName() + ". request로 들어온 파라미터입니다.";
+        } else {
+            return "파라미터값이 없습니다.";
+        }
     }
     
+    static class GreetRequest {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+
+    }
 }
